c06745f267d3710923201b14e06f2e5596ee1788
Zoe Larson <zklars@gmail.com>
2022-02-27 16:55:08 -0600

    navbar added, views/urls added for each page, each page given hello world

--- cryptotutor/urls.py
+++ cryptotutor/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('question/', views.question, name='question'),
+    path('question-form/', views.questionForm, name='question-form'),
+    path('code-form/', views.codeForm, name='code-form'),
+    path('code-selection/', views.codeSelection, name='code-selection'),
+    path('diff-viewer/', views.diffViewer, name='diff-viewer'),
 ]
--- cryptotutor/views.py
+++ cryptotutor/views.py
@@ -4,7 +4,7 @@ import os
 from django.shortcuts import render
 from .models import User
 
-# Create your views here.
+### HOME PAGE ###
 def index(request): 
     """View function for home page of site."""
 
@@ -12,5 +12,60 @@ def index(request):
     f = open(os.getcwd() + "/cryptotutor/static/json/sample_questions.json")
     context = {'questions':json.load(f)}
 
-    #render html page - will need to add context/data once it's retrieved above
+    #render html page
     return render(request, 'index.html', context=context)
+
+
+### QUESTION VIEW ###
+def question(request):
+    """View function for looking at an individual question."""
+
+    #TODO: get whatever is necessary for the page
+    context = {}
+
+    #render html page
+    return render(request, 'question.html', context=context)
+
+
+### QUESTION FORM ###
+def questionForm(request):
+    """View function for asking a question."""
+
+    #TODO: get whatever is necessary for the page
+    context = {}
+
+    #render html page
+    return render(request, 'question-form.html', context=context)
+
+
+### CODE FORM ###
+def codeForm(request):
+    """View function for submitting code for comparison."""
+
+    #TODO: get whatever is necessary for the page
+    context = {}
+
+    #render html page
+    return render(request, 'code-form.html', context=context)
+
+
+### CODE SELECTION ###
+def codeSelection(request):
+    """View function for selecting code for the diff viewer."""
+
+    #TODO: get whatever is necessary for the page
+    context = {}
+
+    #render html page
+    return render(request, 'code-selection.html', context=context)
+
+
+### DIFF VIEWER  ###
+def diffViewer(request):
+    """View function for the diff viewer."""
+
+    #TODO: get whatever is necessary for the page
+    context = {}
+
+    #render html page - will need to add context/data once it's retrieved above
+    return render(request, 'diff-viewer.html', context=context)
