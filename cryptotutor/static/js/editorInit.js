f9706f062c9ab26906f74e3c3d18b67d023d4cd2
Maya Lentsch <davidlentsch@unomaha.edu>
2022-02-28 12:27:40 -0600

    Add Quill & Highlight.js text entry field

---
+++ cryptotutor/static/js/editorInit.js
@@ -0,0 +1,27 @@
+var toolbarOptions = [
+['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+['blockquote', 'code-block'],
+
+[{ 'list': 'ordered'}, { 'list': 'bullet' }],
+[{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+[{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+[{ 'direction': 'rtl' }],                         // text direction
+
+[{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+[{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+[{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+[{ 'font': [] }],
+[{ 'align': [] }],
+
+['clean']                                         // remove formatting button
+];
+
+var quill = new Quill('#editor', {
+  modules: {
+    syntax: true,              // Include syntax module
+    toolbar: toolbarOptions  // Include button in toolbar
+  },
+  theme: 'snow',
+  placeholder: 'Write your question here...'
+});
