af150d222aaa19054a1497ea1e4a22b0d7db3c58
tbauer5011 <tylerbauer@unomaha.edu>
2022-02-28 20:17:33 -0600

    Added relative filepath

--- cryptotutor/models.py
+++ cryptotutor/models.py
@@ -30,7 +30,7 @@ class Nicad(models.Model):
 
     #methods
     def callNicad():
-        subprocess.Popen(["nicad6", "functions", "java", "/home/capstone/Desktop/bit-by-bit-capstone/cryptotutor/ExtraFiles/TestFiles", "default-report"], shell=False)
+        subprocess.Popen(["nicad6", "functions", "java", "../bit-by-bit-capstone/cryptotutor/ExtraFiles/TestFiles", "default-report"], shell=False)
         #file = minidom.parse('')
         return True 
 
