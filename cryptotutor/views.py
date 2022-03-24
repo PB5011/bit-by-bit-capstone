bf6a8d22c219f20eeb47b5d2d4ce9029cc5003ab
tbauer5011 <tylerbauer@unomaha.edu>
2022-03-23 20:51:49 -0500

    Reverting Commit, made an oopsie

--- cryptotutor/views.py
+++ cryptotutor/views.py
@@ -4,7 +4,7 @@ import difflib
 
 from django.shortcuts import render
 #from .models import User,Nicad
-from .models import CodeSubmission, User, Nicad
+from .models import User,Nicad
 
 
 ### HOME PAGE ###
@@ -54,20 +54,6 @@ def codeForm(request):
 
     #TODO: get whatever is necessary for the page
     context = {}
-
-    if request.method == 'POST':
-        x = request.POST['code']
-        #Fixed issue with codesubmission, can now use it for date and names.
-        new_item = CodeSubmission(codeSnippet=x)
-        new_item.save()
-        with open('./cryptotutor/ExtraFiles/SubmittedFiles/Submissions/temp.java', 'w') as f:
-            f.writelines('public class temp { \n')
-            f.writelines('public static void main(String[] args) { \n')
-            f.writelines(new_item.codeSnippet)
-            f.writelines('\n}\n')
-            f.writelines('}')
-            f.close()
-        Nicad.callNicad()
 
     #render html page
     return render(request, 'code-form.html', context=context)
