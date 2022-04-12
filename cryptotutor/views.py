ad92a7f4294a33c0a0e9b3c27cac755be48a47c4
Zoe Larson <zklars@gmail.com>
2022-04-11 19:53:45 -0500

    documentation added to views.py

--- cryptotutor/views.py
+++ cryptotutor/views.py
@@ -8,11 +8,26 @@ from django.shortcuts import render, redirect
 
 from .models import CodeSubmission, User, Nicad, Question
 
+"""
+This file defines all views for the CryptoTutor web application. This is where
+http requests and responses throughout the app go through.
+"""
 
 
 ### HOME PAGE ###
 def index(request): 
-    """View function for home page of site."""
+    """View function for home page of site.
+
+    Currently retrieves questions from a json file of sample questions. This
+    will be updated to pull questions from the client-given database.
+
+    Args:
+        request: A request to view the home page.
+
+    Returns:
+        An HttpResponse with the original request, the home page url, and the
+        context of the page. Context of the page includes the questions from the json file.
+    """
 
     #TODO: get whatever is necessary for the page
     f = open(os.getcwd() + "/cryptotutor/static/json/sample_questions.json")
@@ -27,7 +42,19 @@ def index(request):
 
 ### QUESTION VIEW ###
 def question(request):
-    """View function for looking at an individual question."""
+    """View function for looking at an individual question.
+    
+    Currently retrieves questions from a json file of sample questions and
+    answers. This will be updated to pull questions from the client-given database.
+
+    Args:
+        request: A request to view the individual question's page.
+
+    Returns:
+        An HttpResponse with the original request, the question page url, and the
+        context of the page. Context of the page includes the questions and answers from
+        the json file.
+    """
 
     #TODO: get whatever is necessary for the page
     f = open(os.getcwd() + "/cryptotutor/static/json/sample_question_detail.json")
@@ -42,7 +69,22 @@ def question(request):
 
 ### QUESTION FORM ###
 def questionForm(request):
-    """View function for asking a question."""
+    """View function for asking a question.
+    
+    This is a form for submitting ("asking") a question on the CryptoTutor web
+    application. Questions are submitted through the form to this function, and
+    will eventually be passed to the database.
+    
+    Args:
+        request: A request to view the form to ask a question. Once the user is
+            done with the form and would like to submit their question, the fields
+            are returned to this function as well to store and eventually be put into
+            the database.
+
+    Returns:
+        An HttpResponse with the original request, the question submission page url,
+        and the context of the page. This page requires no context to be rendered. 
+    """
 
     #TODO: get whatever is necessary for the page
     context = {}
@@ -76,7 +118,19 @@ def questionForm(request):
 
 ### CODE FORM ###
 def codeForm(request):
-    """View function for submitting code for comparison."""
+    """View function for submitting code for comparison.
+    
+    This is a form for submitting code to the CryptoTutor code comparison tool.
+
+    Args:
+        request: A request to view the form to submit code. Once the user is done
+            with the form and would like to submit their code, the fields are returned
+            to this function to store in a new file before NiCad is called.
+
+    Returns:
+        An HttpResponse with the original request, the code submission page url, and
+        the context of the page. This page requires no context to be rendered.
+    """
 
     #TODO: get whatever is necessary for the page
     context = {}
@@ -102,7 +156,21 @@ def codeForm(request):
 
 ### CODE SELECTION ###
 def codeSelection(request):
-    """View function for selecting code for the diff viewer."""
+    """View function for selecting code for the diff viewer.
+    
+    This is where users can select which code they would like to compare with
+    their own code submission.
+    
+    Args:
+        request: A request to view the form to select which code to view in the diff
+            viewer. This is also a form that can be submitted; once submitted, the
+            selected code is given to the function to forward to the diff viewer.
+        
+    Returns:
+        An HttpResponse with the original request, the code selection page url, and
+        the context of the page. Context of this page includes the path of the NiCad result
+        file and the parsed result file.
+    """
 
     fileLoc = "/cryptotutor/ExtraFiles/TestFiles_functions-blind-crossclones/TestFiles_functions-blind-crossclones-0.30-classes-withsource.xml"
 
@@ -137,17 +205,20 @@ def codeSelection(request):
 
 ### DIFF VIEWER  ###
 def diffViewer(request):
-    """View function for the diff viewer."""
+    """View function for the diff viewer.
+    
+    This is where users can see the differences between their submitted code and
+    the code they selected on the code selection page.
 
-    #test files to make sure view/diff works
-    #file1file = os.getcwd() + "/cryptotutor/static/json/sample_question_detail.json"
-    #file2file = os.getcwd() + "/cryptotutor/static/json/sample_question_detail_diff.json"
+    Args:
+        request: A request to view the diff viewer with submitted and selected code.
 
-    #file1lines = open(os.getcwd() + "/cryptotutor/static/json/sample_question_detail.json",'U').readlines()
-    #file2lines = open(os.getcwd() + "/cryptotutor/static/json/sample_question_detail_diff.json",'U').readlines()
+    Returns:
+        An HttpResponse with the original request, the diff viewer page url, and the
+        context of the page. The context of this page includes the submitted file, the
+        selected file, and the diff view comparing them.
+    """
 
-
-    #submitted files - need to test
     file1file = os.getcwd() + "/cryptotutor/ExtraFiles/SubmittedFiles/Submissions/temp.java"
     file2file = os.getcwd() + request.session['compareFile']
 
@@ -166,6 +237,21 @@ def diffViewer(request):
 
 ### NICAD RESULTS  ###
 def nicadResults(request):
+    """View function for the NiCad results.
+
+    This function is mostly intended for developer use. It displays the results
+    from a NiCad run as given by the relevant file.
+
+    Args:
+        request: A request to view the results of a NiCad run.
+        
+    Returns:
+        An HttpResponse with the original request, the NiCad results url, and
+        the context of the page. Context of this page includes the path of the NiCad result
+        file and the parsed result file.
+    
+    """
+
     fileLoc = '/' + request.GET.get('file', '/cryptotutor/ExtraFiles/TestFiles_functions-blind-crossclones/TestFiles_functions-blind-crossclones-0.30-classes-withsource.xml')
 
     f = open(os.getcwd() + fileLoc)
