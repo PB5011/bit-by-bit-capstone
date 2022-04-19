3c0125fd146d994b5c2cf784ebfb0f21d71eb1c2
Maya Lentsch <davidlentsch@unomaha.edu>
2022-04-18 20:21:26 -0500

    Work in progress: login system

--- cryptotutor/urls.py
+++ cryptotutor/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -8,6 +8,5 @@ urlpatterns = [
     path('code-form/', views.codeForm, name='code-form'),
     path('code-selection/', views.codeSelection, name='code-selection'),
     path('diff-viewer/', views.diffViewer, name='diff-viewer'),
-    path('results/', views.nicadResults, name='nicad-results'),
-    path('login/', views.login, name='login')
+    path('results/', views.nicadResults, name='nicad-results')
 ]
