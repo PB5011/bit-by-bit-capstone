567d18db3cf132e082b347996cf928f171596ccc
Daniel Brinkman <53248112+danielbrinkman@users.noreply.github.com>
2022-04-20 19:16:27 -0500

    updated with new question format, added defaults

--- cryptotutor/models.py
+++ cryptotutor/models.py
@@ -3,20 +3,21 @@ from django.urls import reverse
 import subprocess
 from xml.dom import minidom
 
+
 # Create your models here.
 class User(models.Model):
     """Class defining a user of CryptoTutor"""
 
-    #fields
-    #TODO: flesh these out past the very basics
+    # fields
+    # TODO: flesh these out past the very basics
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=50)
 
-    #metadata
+    # metadata
     class Meta:
         ordering = ['username', 'password']
 
-    #methods
+    # methods
     def get_absolute_url(self):
         """returns the url to access a particular instance of User"""
         return reverse('model-detail-view', args=[str(self.id)])
@@ -25,57 +26,65 @@ class User(models.Model):
         """String for representing the User object (in Admin site etc.)"""
         return self.username + self.password
 
+
 class Nicad(models.Model):
     """Class defining nicad use?"""
 
-    #methods
+    # methods
     def callNicad():
-        subprocess.Popen(["nicad6cross", "blocks", "java", "./cryptotutor/ExtraFiles/SubmittedFiles/Submissions", "./cryptotutor/ExtraFiles/TestFiles", "default-report"], shell=False)
-        return True 
+        subprocess.Popen(["nicad6cross", "blocks", "java", "./cryptotutor/ExtraFiles/SubmittedFiles/Submissions",
+                          "./cryptotutor/ExtraFiles/TestFiles", "default-report"], shell=False)
+        return True
+
 
 class CodeSubmission(models.Model):
     codeSnippet = models.TextField()
 
-class Question(models.Model):
 
+class Question(models.Model):
     # CATEGORIES = (
     #     ('T1', 'Tag1'),
     #     ('T2', 'Tag2'),
     #     ('T3', 'Tag3'),
-    #format: ('InternalVariableName', 'UIButtonName'),
+    # format: ('InternalVariableName', 'UIButtonName'),
     # ) # change as needed, not implemented yet
-    StudentID = models.CharField(max_length=10) # probs should be intege>
+    StudentID = models.CharField(max_length=10)  # probs should be intege>
     StudentName = models.CharField(max_length=50)
     projectLink = models.CharField(max_length=50)
-    description = models.CharField(max_length=100)
-    
-    
-    
+    description = models.TextField()
+    title = models.CharField(max_length=100, default='test')
+    points = models.IntegerField(default='0')
+    responses = models.PositiveSmallIntegerField(default='0')
+
+
 
 class inheritedQuestion(models.Model):
     codeFragment = models.CharField(max_length=2048)
     dataTime = models.CharField(max_length=255)
     fileName = models.CharField(max_length=255)
     question = models.CharField(max_length=255)
-    studentID = models.IntegerField(max_length=10)
+    studentID = models.IntegerField()
     username = models.CharField(max_length=255)
     threshold = models.CharField(max_length=255)
     misuse = models.CharField(max_length=255)
+
 
 class student(models.Model):
     address = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
 
+
 class inheritedUser(models.Model):
     password = models.CharField(max_length=255)
-    schoolID = models.IntegerField(max_length=10)
-    userType = models.IntegerField(max_length=10)
+    schoolID = models.IntegerField()
+    userType = models.IntegerField()
     username = models.CharField(max_length=255)
+
 
 class answers(models.Model):
     answer = models.CharField(max_length=255)
-    questionID = models.IntegerField(max_length=10)
-    studentID = models.IntegerField(max_length=10)
+    questionID = models.IntegerField()
+    studentID = models.IntegerField()
     username = models.CharField(max_length=255)
 
 
