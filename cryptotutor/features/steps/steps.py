0ee5ac8a7ce17eb0656cbe38a376f82100bc4795
Zoe Larson <zklars@gmail.com>
2022-04-20 19:56:04 -0500

    basic front end testing

--- cryptotutor/features/steps/steps.py
+++ cryptotutor/features/steps/steps.py
@@ -1,33 +1,54 @@
 from behave import *
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 
 driver = webdriver.Firefox(executable_path=r'/usr/local/bin/geckodriver') #geckodriver MUST be inside the /usr/local/bin folder
 
-#Feature: showing off behave
-@given('we have behave installed')
-def step_impl(context):
-    pass
+def setUp(context):
+    context.driver = driver
 
-@when('we implement a test')
-def step_impl(context):
-    assert True is not False
-
-@then('behave will test it for us!')
-def step_impl(context):
-    assert context.failed is False
-
-
-#Feature: checking links from home
 @given('I go to the cryptotutor home page')
 def step_impl(context):
     driver.get('http://localhost:8000/cryptotutor')
 
 @when('I click on the home link')
 def step_impl(context):
-    driver.find_elements_by_css_selector('mdc-deprecated-list-item')
+    WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/aside/div[2]/nav/a[1]")))
+    driver.find_element_by_xpath("/html/body/div[1]/aside/div[2]/nav/a[1]").click()
 
 @then('I will go to the home page')
 def step_impl(context):
-    assert driver.current_url == "http://localhost:8000/cryptotutor"
+    assert driver.current_url == "http://localhost:8000/cryptotutor/"
 
-driver.close()
+@when('I click on the question submission link')
+def step_impl(context):
+    WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/aside/div[2]/nav/a[2]")))
+    driver.find_element_by_xpath("/html/body/div[1]/aside/div[2]/nav/a[2]").click()
+
+@then('I will go to the question submission page')
+def step_impl(context):
+    assert driver.current_url == "http://localhost:8000/cryptotutor/question-form/"
+
+@when('I click on the compare code link')
+def step_impl(context):
+    WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/aside/div[2]/nav/a[3]")))
+    driver.find_element_by_xpath("/html/body/div[1]/aside/div[2]/nav/a[3]").click()
+
+@then('I will go to the code comparison form')
+def step_impl(context):
+    assert driver.current_url == "http://localhost:8000/cryptotutor/code-form/"
+
+@when('I click on the GitHub link')
+def step_impl(context):
+    WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/aside/div[2]/nav/a[4]")))
+    driver.find_element_by_xpath("/html/body/div[1]/aside/div[2]/nav/a[4]").click()
+
+@then('I will go to the GitHub')
+def step_impl(context):
+    assert driver.current_url == "https://github.com/davidlentsch/bit-by-bit-capstone"
+
+# def tearDown(context):
+    driver.close()
