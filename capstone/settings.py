1f7f8a853251d74cea43dee81a6a73bf72f94a15
Maya Lentsch <davidlentsch@unomaha.edu>
2022-04-25 19:48:24 -0500

    Tweak login and logout forms

--- capstone/settings.py
+++ capstone/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cryptotutor.apps.CryptotutorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cryptotutor.apps.CryptotutorConfig',
     'rest_framework'
 ]
 
@@ -125,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
